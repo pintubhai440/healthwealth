f51707bb020c63074b1b64fa64d0eb392b88576d
pintubhai440 <p7431309@gmail.com>
2025-12-09 20:25:01 +0530

    Update TriageBot.tsx

--- components/TriageBot.tsx
+++ components/TriageBot.tsx
@@ -186,7 +186,7 @@ export const TriageBot: React.FC = () => {
       <div className="bg-teal-600 p-4 text-white flex items-center justify-between">
         <div className="flex items-center gap-2">
             <Stethoscope className="w-6 h-6" />
-            <h2 className="font-bold text-lg">Smart Triage</h2>
+            <h2 className="font-bold text-lg">Smart haiTriage</h2>
         </div>
         <div className="flex items-center gap-3">
              {isSpeaking && (
