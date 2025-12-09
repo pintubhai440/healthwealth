acb80c04ae9bdfff83801a4dcc31feae89a6c07d
pintubhai440 <p7431309@gmail.com>
2025-12-09 23:28:52 +0530

    Update gemini.ts

--- services/gemini.ts
+++ services/gemini.ts
@@ -170,7 +170,7 @@ export const generateTTS = async (text: string) => {
   try {
     const client = getGenAIClient(); // Random Key
     const response = await client.models.generateContent({
-      model: 'gemini-2.5-flash-preview-tts', 
+      model: 'gemini-2.5-flash-lite', 
       contents: { parts: [{ text }] },
       config: {
         responseModalities: [Modality.AUDIO],
