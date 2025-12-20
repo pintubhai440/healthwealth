6ae610bf69579e141dd6bd135b4fb87b531b8cfa
pintubhai440 <p7431309@gmail.com>
2025-12-20 13:45:50 +0530

    Update types.ts

--- types.ts
+++ types.ts
@@ -5,7 +5,8 @@ export enum FeatureView {
   DERMCHECK = 'DERMCHECK',
   RECOVERY = 'RECOVERY',
   GUARDIAN = 'GUARDIAN', // ✅ Ye line add karein (5th Box ke liye)
-  REPORT = 'REPORT' // ✅ ये लाइन जोड़ें (Lab Reports के लिए)
+  REPORT = 'REPORT', // ✅ ये लाइन जोड़ें (Lab Reports के लिए)
+  TRANSLATOR = 'TRANSLATOR'
 }
 
 export interface PatientProfile {
